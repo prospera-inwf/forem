07045a06e46e5e4029efcf126000a84fdb6dd6c4
mclopez10 <109301771+mclopez10@users.noreply.github.com>
2026-02-06 11:19:07 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,6 +129,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: protocol + ENV["APP_DOMAIN"].to_s }
+  config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.smtp_settings = {
+  address: ENV["SMTP_ADDRESS"],
+  port: ENV.fetch("SMTP_PORT", "587").to_i,
+  domain: ENV["SMTP_DOMAIN"],
+  user_name: ENV["SMTP_USER_NAME"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: (ENV["SMTP_AUTHENTICATION"] || "plain").to_sym,
+  enable_starttls_auto: ENV.fetch("SMTP_ENABLE_STARTTLS_AUTO", "true") == "true"
+}
 
   if ENV["HEROKU_APP_URL"].present? && ENV["HEROKU_APP_URL"] != ENV["APP_DOMAIN"]
     config.middleware.use Rack::HostRedirect,
